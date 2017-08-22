48b0abef853a57fcfaa29f94f11526d83246b105
Tomas <tomasp1189@gmail.com>
2017-08-22 08:40:12 -0300

    Commit oauth implemented and first mongoDB

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
+
+var options = {
+	server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+	replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+
+mongoose.connect(keys.mongoURI, options);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'heey' });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
